3943d5cae15218304d5bd4943ae24ff3d4d009a4
danlo <danlo12@mail.ru>
2023-02-27 22:13:21 +0300

    step 2

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,6 +6,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument("second_file")
+    parser.add_argument('-f','--format', dest='format', help='sum the integers (default: find the max)')
     args = parser.parse_args()
     return args.accumulate(args.integers)
 
