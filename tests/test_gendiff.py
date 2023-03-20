43689fc73c246f2ce6445e2390f716ad9174f2a1
danlo <danlo12@mail.ru>
2023-03-20 22:11:28 +0300

    s 5

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,12 +3,4 @@ from gendiff import generate_diff
 
 
 def test_gendiff():
-    file_list = {
-        '- follow': False,
-        '  host': 'hexlet.io',
-        '- proxy': '123.234.53.22',
-        '- timeout': 50,
-        '+ timeout': 20,
-        '+ verbose': True}
-    assert generate_diff.mk_str(file_list) == right_result.right()
     assert right_result.right() == generate_diff.generate_result('tests/fixturs/file1.json', 'tests/fixturs/file2.json')
