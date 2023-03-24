8c24000a32898389a0752a4698008169ed1dc488
danlo <danlo12@mail.ru>
2023-03-24 22:18:02 +0300

    step 5:splitting a function into modules

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -6,7 +6,7 @@ def stulish(result, lvl=2):
     for str_result in result:
         if result[str_result] is True or result[str_result] is False:
             result[str_result] = str(result[str_result]).lower()
-        if result[str_result] is None:
+        elif result[str_result] is None:
             result[str_result] = "null"
         else:
             result[str_result] = str(result[str_result])
@@ -15,8 +15,7 @@ def stulish(result, lvl=2):
     return final
 
 
-def generate(file1, file2, lvl=2):
-    result = dict()
+def generate_dict(file1, file2):
     common = list()
     n_common = list()
     for key1 in file1:
@@ -29,6 +28,12 @@ def generate(file1, file2, lvl=2):
             continue
         else:
             n_common.append(key2)
+    return common, n_common
+
+
+def generate(file1, file2, lvl=2):
+    result = dict()
+    common, n_common = generate_dict(file1, file2)[0], generate_dict(file1, file2)[1]
     for key_all in common:
         if type(file1[key_all]) is dict and type(file2[key_all]) is dict:
             result[(" " * lvl) + "  " + key_all] = stulish(generate(file1[key_all], file2[key_all], lvl + 4), lvl + 2)
@@ -43,23 +48,24 @@ def generate(file1, file2, lvl=2):
             result[(" " * lvl) + "+ " + key_all] = file2[key_all]
         else:
             result[(" " * lvl) + "  " + key_all] = file1[key_all]
-    for key_nall in n_common:
-        if key_nall in file1:
-            if type(file1[key_nall]) is dict:
-                result[(" " * lvl) + "- " + key_nall] = stulish(generate(file1[key_nall],
-                                                                        file1[key_nall], lvl + 4), lvl + 2)
-            else:
-                result[(" " * lvl) + "- " + key_nall] = file1[key_nall]
-        elif key_nall in file2:
-            if type(file2[key_nall]) is dict:
-                result[(" " * lvl) + "+ " + key_nall] = stulish(generate(file2[key_nall],
-                                                                        file2[key_nall], lvl + 4), lvl + 2)
-            else:
-                result[(" " * lvl) + "+ " + key_nall] = file2[key_nall]
+    timer = 0
+    for file_value in (file1, file2):
+        timer += 1
+        for key in n_common:
+            if key in file_value:
+                if type(file_value[key]) is dict and timer == 1:
+                    result[(" " * lvl) + "- " + key] = stulish(generate(file_value[key], file_value[key], lvl + 4), lvl + 2)
+                elif type(file_value[key]) is dict and timer == 2:
+                    result[(" " * lvl) + "+ " + key] = stulish(generate(file_value[key], file_value[key], lvl + 4), lvl + 2)
+                else:
+                    if timer == 1:
+                        result[(" " * lvl) + "- " + key] = file_value[key]
+                    else:
+                        result[(" " * lvl) + "+ " + key] = file_value[key]
     return result
 
 
-def return_result(file_path1, file_path2,formater="stylish"):
+def return_result(file_path1, file_path2, formater="stylish"):
     file1 = definition_form(file_path1)
     file2 = definition_form(file_path2)
     if formater == "stylish":
