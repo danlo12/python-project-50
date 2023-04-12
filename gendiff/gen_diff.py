cd60e3a805f5616be8320d2f31ad4eb800c4ddce
danlo <danlo12@mail.ru>
2023-04-12 14:50:41 +0300

    add "default" arg

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -3,7 +3,7 @@ from gendiff.generate_stylish import generate, stulish
 from gendiff.gen_plain import plain
 
 
-def generate_diff(file_path1, file_path2, formater="STYLISH"):
+def generate_diff(file_path1, file_path2, formater):
     file1 = definition_form(file_path1)
     file2 = definition_form(file_path2)
     if formater == "STYLISH":
