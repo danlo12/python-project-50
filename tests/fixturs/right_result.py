bcb1f6263b4f9ef8923c224369ab3120f25478d6
danlo <danlo12@mail.ru>
2023-05-01 16:32:01 +0300

    add test

--- tests/fixturs/right_result.py
+++ tests/fixturs/right_result.py
@@ -1,5 +1,5 @@
 from gendiff import generate_stylish
-from gendiff import gen_diff
+from gendiff import generate_json
 file = {
     "    host": "hexlet.io",
     "  - timeout": "50",
@@ -13,3 +13,5 @@ file = {
 def right_stylish():
     return generate_stylish.stulish(file)
 
+def right_json():
+    return generate_json.json_s(file)
