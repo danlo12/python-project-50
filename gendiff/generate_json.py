3b1dfe54d829f77eee8771ee2afd805dbe4a3c7a
danlo <danlo12@mail.ru>
2023-05-01 19:23:48 +0300

    json test up

--- gendiff/generate_json.py
+++ gendiff/generate_json.py
@@ -2,4 +2,7 @@ import json
 
 
 def json_s(result):
+    for arg in result:
+        if result[arg] is True or result[arg] is False:
+            result[arg] = str(result[arg]).lower()
     return json.dumps(result)
