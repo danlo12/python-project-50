c0992cd6cc809236ae7611d5b93e5fb508ff410d
danlo <danlo12@mail.ru>
2023-05-06 17:47:06 +0300

    st 8

--- tests/fixturs/test_json.py
+++ tests/fixturs/test_json.py
@@ -1,4 +1,5 @@
-import json
 from gendiff import gen_diff
+
+
 def test_json():
-    gen_diff.generate_diff("tests/fixturs/file1.json", "tests/fixturs/file2.json","JSON") == {"    host": "hexlet.io", "  - timeout": 50, "  + timeout": 20, "  - follow": "false", "  - proxy": "123.234.53.22", "  + verbose": "true"}
+    assert gen_diff.generate_diff("tests/fixturs/file1.json", "tests/fixturs/file2.json", "JSON") == {"    host": "hexlet.io", "  - timeout": 50, "  + timeout": 20, "  - follow": "false", "  - proxy": "123.234.53.22", "  + verbose": "true"}
