8816fb6ac3d258add0e859e940092f087d865bab
danlo <danlo12@mail.ru>
2023-05-09 13:20:31 +0300

    refactoring tree

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 import argparse
-from gendiff import generate_diff
+from gendiff.parser import definition_form
+from gendiff.generate_stylish import generate, stulish
+from gendiff.gen_plain import plain
+from gendiff.generate_json import json_s
 
 
+def generate_diff(file_path1, file_path2, formater="STYLISH"):
+    file1 = definition_form(file_path1)
+    file2 = definition_form(file_path2)
+    if formater == "STYLISH":
+        return stulish(generate(file1, file2))
+    if formater == "PLAIN":
+        return plain(file1, file2)
+    if formater == "JSON":
+        return json_s(generate(file1, file2))
 def main():
     desctipt = 'Compares two configuration files and shows a difference.'
     parser = argparse.ArgumentParser(description=desctipt)
@@ -11,7 +23,7 @@ def main():
     f_help = 'set format of output'
     parser.add_argument('-f', '--format', dest='format', help=f_help, default="STYLISH")
     args = parser.parse_args()
-    return generate_diff.generate_diff(args.first_file, args.second_file, args.format)
+    return generate_diff(args.first_file, args.second_file, args.format)
 
 
 if __name__ == '__main__':
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,6 +1,6 @@
-from gendiff import generate_diff
+from gendiff.scripts import gendiff
 import json
 
 
 def test_json():
-    assert generate_diff.generate_diff("tests/fixturs/file1.json", "tests/fixturs/file2.json", "JSON") == json.dumps({"    host": "hexlet.io", "  - timeout": 50, "  + timeout": 20, "  - follow": "false", "  - proxy": "123.234.53.22", "  + verbose": "true"})
+    assert gendiff.generate_diff("tests/fixturs/file1.json", "tests/fixturs/file2.json", "JSON") == json.dumps({"    host": "hexlet.io", "  - timeout": 50, "  + timeout": 20, "  - follow": "false", "  - proxy": "123.234.53.22", "  + verbose": "true"})
