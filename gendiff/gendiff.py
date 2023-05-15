b577bbf6e8e909bce2e1041b64fbe3e98df88b4a
danlo <danlo12@mail.ru>
2023-05-16 01:52:53 +0300

    formatter call change

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -4,12 +4,13 @@ from gendiff.gen_plain import plain
 from gendiff.generate_json import json_s
 
 
-def generate_diff(file_path1, file_path2, formater="STYLISH"):
+def generate_diff(file_path1, file_path2, formater="stylish"):
     file1 = definition_form(file_path1)
     file2 = definition_form(file_path2)
-    if formater == "STYLISH":
+    formater = formater.lower()
+    if formater == "stylish":
         return stulish(generate(file1, file2))
-    if formater == "PLAIN":
+    if formater == "plain":
         return plain(file1, file2)
-    if formater == "JSON":
+    if formater == "json":
         return json_s(generate(file1, file2))
